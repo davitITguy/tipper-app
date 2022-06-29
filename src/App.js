941ea9666fccbdce6b0aaef4c77ed0da8b966604
Davit Spanderashvili <37460419+davitITguy@users.noreply.github.com>
2022-06-29 21:14:39 +0400

    tipper demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import mobile from "./assets/images/Group12201.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="body">
+        <h1>TIPPR</h1>
+        <h3>Send tips, with in a few taps</h3>
+        <img src={mobile} />
+        <div className="btns">
+          <button className="btn btn-dark">Continue with app</button>
+          <button className="btn btn-light">Continue as guest</button>
+        </div>
+      </div>
     </div>
   );
 }
